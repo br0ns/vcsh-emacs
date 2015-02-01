8be163f2c81df1b5e85e736e49091f52a64a94dd
Mathias Svensson <idolf@pwnies.dk>
2015-02-01 21:24:41 +0100

    Agda options

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -77,3 +77,12 @@
 
 (load-file (let ((coding-system-for-read 'utf-8))
                 (shell-command-to-string "agda-mode locate")))
+(custom-set-variables
+ '(agda2-include-dirs (quote ("." "/home/freaken/git/agda-stdlib/src"))))
+(custom-set-faces
+ '(agda2-highlight-datatype-face ((t (:foreground "deep sky blue"))))
+ '(agda2-highlight-function-face ((t (:foreground "deep sky blue"))))
+ '(agda2-highlight-postulate-face ((t (:foreground "deep sky blue"))))
+ '(agda2-highlight-primitive-face ((t (:foreground "deep sky blue"))))
+ '(agda2-highlight-primitive-type-face ((t (:foreground "deep sky blue"))))
+ '(agda2-highlight-record-face ((t (:foreground "deep sky blue")))))
